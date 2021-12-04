a5f1dd9c727b9185570614a39bfd7c20e149e3e5
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-04 17:12:02 +0000

    to continue

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_164532) do
+ActiveRecord::Schema.define(version: 2021_12_04_171125) do
 
   create_table "organisations", force: :cascade do |t|
     t.string "name"
     t.decimal "hourly_rate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "start"
+    t.index ["user_id"], name: "index_shifts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_12_04_164532) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shifts", "users"
 end
